4831e8e4e29b2445a17f7a85018b71256918558f
cy-chow <carlchow22@gmail.com>
2020-06-28 00:52:00 -0700

    set up message

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
-# Identify C Declarations
-This program takes in identifies the type of C declaration that a user
-inputs to the command line or inside a file.
-Try running
+# Identify 
+This program accepts C declarations that a user inputs to the command line 
+or inside a file and translate them to plain English
+<p>Try running
     
     python3 main.py
 
-to get started.
+to get started.</p>
 
-**Note** 
+**Notes** 
  - This program does not handle unsigned data type declarations.
  - The identifier for the declaration must be *x*, but you can change this in *definition.txt*
  - *example.txt* provides sample C declarations to parse.
--- identify.py
+++ identify.py
@@ -1,8 +1,23 @@
 from definition import * 
 
 def identify(lines):
+  """
+  This function evaluates a list of C declaration strings and prints the result.
+  """
   for line in lines:
-    line = line.replace(' ', '')
-    for datatype in type_dict:
-      line = line.replace(datatype, type_dict[datatype])
-    print(line)
+    print(line + ':', end = ' ')
+    ans = identifier + ' is a'
+    line = replace(line)
+    print(ans)
+
+def replace(line):
+  for datatype in type_dict:
+    line = line.replace(datatype, type_dict[datatype])
+  line = line.replace(' ', '')
+  return line
+
+def array(substr):
+  return 0
+
+def function(substr):
+  return 0
