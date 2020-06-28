e4c816ce7b1c31e7a3acdbdc841a65c09eededc9
cy-chow <carlchow22@gmail.com>
2020-06-28 01:57:09 -0700

    added input checks

--- identify.py
+++ identify.py
@@ -5,12 +5,24 @@ def identify(lines):
   This function evaluates a list of C declaration strings and prints the result.
   """
   for line in lines:
+    # set up the line for parsing
     print(line + ':', end = ' ')
-    ans = identifier + ' is a'
-    line = replace(line)
-    print(ans)
+    line = proofread(line)
 
-def replace(line):
+    ans = identifier + ' is a'
+    index = 0
+    try:
+      index = line.index(identifier)
+      while index != 0:
+        index = 0
+      print(ans + inv_dict[line[0]])
+    except ValueError: 
+      print('Invalid identifier ' + 
+          '(make sure your declaration includes ' + identifier + ')')
+    except KeyError:
+      print('Invalid data type used')
+
+def proofread(line):
   for datatype in type_dict:
     line = line.replace(datatype, type_dict[datatype])
   line = line.replace(' ', '')
