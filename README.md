7e82f6fb84dee14580834499ed259335bb479981
cy-chow <carlchow22@gmail.com>
2020-06-29 00:09:45 -0700

    First iteration pushed

--- README.md
+++ README.md
@@ -1,13 +1,35 @@
 # Identify 
 This program accepts C declarations that a user inputs to the command line 
-or inside a file and translate them to plain English
+or inside a file and translate them to plain English.
+My primary reference for C declarations can be found [here](https://parrt.cs.usfca.edu/doc/how-to-read-C-declarations.html) 
 <p>Try running
     
     python3 main.py
 
 to get started.</p>
 
-**Notes** 
+## Example Output
+*example.txt*:
+  1. short x                                                                          
+  2. long x                                                                           
+  3. char x[]                                                                         
+  4. int (\*x)[]                                                                       
+  5. void x(long,int\*)                                                                
+  6. int (\*(\*x)[])()                                                                  
+  7. int (\*x[])()                                                                     
+  8. long (\*x())[256]
+
+Output:
+    short x: x is short
+    long x: x is long
+    char x[]: x is an array of char
+    int (*x)[]: x is pointer to an array of int
+    void x(long,int*): x is function taking long and int pointer returning void
+    int (*(*x)[])(): x is pointer to an array of pointer to function returning int
+    int (*x[])(): x is an array of pointer to function returning int
+    long (*x())[256]: x is function returning pointer to an array of size 256 of long  
+
+## Notes 
  - This program does not handle unsigned data type declarations.
  - The identifier for the declaration must be *x*, but you can change this in *definition.txt*
  - *example.txt* provides sample C declarations to parse.
--- identify.py
+++ identify.py
@@ -2,20 +2,63 @@ from definition import *
 
 def identify(lines):
   """
-  This function evaluates a list of C declaration strings and prints the result.
+  This function evaluates a list of C declaration strings 
+  and prints the result.
   """
   for line in lines:
     # set up the line for parsing
-    print(line + ':', end = ' ')
     line = proofread(line)
 
-    ans = identifier + ' is a'
-    index = 0
+    ans = identifier + ' is '
     try:
-      index = line.index(identifier)
-      while index != 0:
-        index = 0
+      start = line.index(identifier) - 1
+      end = start + 2
+      while start > 0 or end < len(line):
+        # Check end index
+        if end >= len(line):
+          pass
+        # Identify functions
+        elif line[end] == '(':
+          ans += 'function '
+          end += 1
+          if line[end] != ')':
+            ans += 'taking '
+            while line[end] != ')':
+              if line[end] == ',':
+                ans += 'and '
+              elif line[end] == '*':
+                ans += 'pointer '
+              else:
+                ans += inv_dict[line[end]] + ' '
+              end += 1
+
+            # Identify the parameters of the function
+            while line[end] != ')':
+              end += 1
+          ans += 'returning '
+        # Identify arrays
+        elif line[end] == '[':
+          end += 1
+          ans += 'an array of '
+          if line[end] != ']':
+            ans += 'size '
+            while line[end] != ']':
+              ans += line[end]
+              end += 1
+            ans += ' of '
+        
+        # Check start index
+        if start <= 0:
+          pass
+        # Identify pointers
+        elif line[start] == '*':
+          ans += 'pointer to '
+        
+        # Move start and end indices outward
+        start -= 1
+        end += 1
       print(ans + inv_dict[line[0]])
+    
     except ValueError: 
       print('Invalid identifier ' + 
           '(make sure your declaration includes ' + identifier + ')')
@@ -23,13 +66,8 @@ def identify(lines):
       print('Invalid data type used')
 
 def proofread(line):
+  print(line + ':', end = ' ')
   for datatype in type_dict:
     line = line.replace(datatype, type_dict[datatype])
   line = line.replace(' ', '')
   return line
-
-def array(substr):
-  return 0
-
-def function(substr):
-  return 0
